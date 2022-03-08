b4d8a9a16ab3a165e64f26195bb912c38e4c96bc
Wanitcha <u6213365@au.edu>
2022-03-08 12:07:08 +0700

    ReactApp deployment

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.static(path.join(__dirname, "public", "react-quotation")));
 app.get("/react-quotation/*", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "react-quotation", "index.html"));
