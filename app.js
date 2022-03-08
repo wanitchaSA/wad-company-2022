8529269db379156f8d7addbf6f7323117e272f17
Wanitcha <u6213365@au.edu>
2022-03-08 11:22:32 +0700

    ReactApp deployment

--- app.js
+++ app.js
@@ -25,10 +25,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public", "react-quotation")));
 
-app.get('/react-quotation', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'react-quotation', index.html));
+app.get('/react-quotation/*', function(req, res) {
+  res.sendFile(path.join(__dirname, "public", "react-quotation", "index.html"));
 });
 
 // Plug routers
